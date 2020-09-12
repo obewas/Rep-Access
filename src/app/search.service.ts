f096a61f306c023e1fab9aa81e0bbbe30859ed5b
obewas <obewas1202@gmail.com>
2020-09-12 17:38:15 +0300

    added selfrepo component

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,20 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable, empty, observable, of } from "rxjs";
+import { Observable, empty } from "rxjs";
 import { Githubuser } from "./githubuser";
-import { catchError, map, tap } from "rxjs/operators";
 
 
 @Injectable()
 export class SearchService {
-  private handleError:any
   public searchResults:any;
   private finaldata = []
   private apiUrl:string = "https://api.github.com/users"
   constructor(private http:HttpClient) { }
-
-  
-
   getData():Observable<Githubuser[]>{
      return this.http.get<Githubuser[]>(this.apiUrl)
   }
@@ -22,9 +17,7 @@ export class SearchService {
     const url = `${this.apiUrl}/${login}`;
     return this.http.get<Githubuser>(url)
   }
-  
     
- 
-  }
-    
-  
+
+
+} 
--- src/app/selfrepo/selfrepo.component.ts
+++ src/app/selfrepo/selfrepo.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { SearchService } from "../search.service";
+import {  Githubuser } from '../githubuser'
 
 @Component({
   selector: 'app-selfrepo',
   templateUrl: './selfrepo.component.html',
   styleUrls: ['./selfrepo.component.css']
 })
-export class SelfrepoComponent implements OnInit {
 
+export class SelfrepoComponent implements OnInit {
+  @Input() githubuser: Githubuser
   constructor(private searchService:SearchService) {  }
    
   ngOnInit(): void {
